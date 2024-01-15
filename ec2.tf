7330dd176c82b7ed43390446b7b710331d2d904e
Park Jinhong <43333982+95jinhong@users.noreply.github.com>
2024-01-16 01:17:22 +0900

    chore: output public ip 추가

--- ec2.tf
+++ ec2.tf
@@ -10,3 +10,7 @@ resource "aws_instance" "ec2" {
 output "ec2_ip" {
     value = aws_instance.ec2.private_ip
 }
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.ec2.public_ip
+}
