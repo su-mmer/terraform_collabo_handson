dfd0897d48e831b7512aa38304de0740f30704a3
95jinhong <95park@naver.com>
2024-02-13 15:11:29 +0900

    fix: ec2 제거

--- ec2.tf
+++ ec2.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "ec2" {
-  ami = "ami-0a5a6128e65676ebb"
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "ec2" {
+#   ami = "ami-0a5a6128e65676ebb"
+#   instance_type = "t2.micro"
+# }
 
-output "ec2_ip" {
-    value = aws_instance.ec2.private_ip
-}
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.ec2.public_ip
-}
+# output "ec2_ip" {
+#     value = aws_instance.ec2.private_ip
+# }
+# output "instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.ec2.public_ip
+# }
